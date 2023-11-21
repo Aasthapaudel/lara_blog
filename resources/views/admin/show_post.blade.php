a6c643ff691ebed4e8f04f2f0b893d68032f782f
Aastha <pdlpangali111@gmail.com>
2023-11-21 19:04:59 +0545

    sweet alert'

--- resources/views/admin/show_post.blade.php
+++ resources/views/admin/show_post.blade.php
@@ -1,55 +1,60 @@
 <!DOCTYPE html>
 <html>
-  <head>
-   @include('admin.css')
-   <style type="text/css">
-    .title_deg{
-font-size:30px;
-font-weight:bold;
-color:white;
-padding:30px;
-text-align:center;
-    }
-    .table_deg{
-        border:2px solid white;
-        width:100%;
-        text-align:center;
-        margin-left:80px;
-        padding:10px;
-    }
-    .th_dg{
-        border:1px solid white;
 
-        background-color:skyblue;
+<head>
+    @include('admin.css')
+    <style type="text/css">
+        .title_deg {
+            font-size: 30px;
+            font-weight: bold;
+            color: white;
+            padding: 30px;
+            text-align: center;
+        }
 
-    }
-    .img_deg{
-        height:100px;
-        width:150px;
-        padding:10px;
+        .table_deg {
+            border: 2px solid white;
+            width: 100%;
+            text-align: center;
+            margin-left: 80px;
+            padding: 10px;
+        }
 
-    }
-   </style>
-  </head>
-  <body>
+        .th_dg {
+            border: 1px solid white;
+
+            background-color: skyblue;
+
+        }
+
+        .img_deg {
+            height: 100px;
+            width: 150px;
+            padding: 10px;
+
+        }
+    </style>
+</head>
+
+<body>
     <header class="header">
-      @include('admin.header')
+        @include('admin.header')
     </header>
     <div class="d-flex align-items-stretch">
-      <!-- Sidebar Navigation-->
-      @include('admin.sidebar')
+        <!-- Sidebar Navigation-->
+        @include('admin.sidebar')
+    </div>
+
+    <div class="page-content">
+        @if(session()->has('message'))
+        <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+            {{session()->get('message')}}
         </div>
+        @endif
+        <h1 class="title_deg">All Post</h1>
 
-        <div class="page-content">
-@if(session()->has('message'))
-<div class ="alert alert-danger">
-    <button type="button" class ="close" data-dismiss="alert" aria-hidden="true">x</button>
-    {{session()->get('message')}}
-</div>
-@endif
-          <h1 class ="title_deg">All Post</h1>
-
-          <table class="table_deg">
+        <table class="table_deg">
             <tr class="th_dg">
                 <th>Post title</th>
                 <th>Description</th>
@@ -70,10 +75,13 @@ text-align:center;
                     <img class="img_deg" src="postimage/{{$post->image}}">
                 </td>
                 <td>
-                    <a href="{{url('delete_post',$post->id)}}" class="btn btn-danger" onclick="return confirm('Are you sure to Delete This ?')">Delete</a>
+                    <!-- <a href="{{url('delete_post',$post->id)}}" class="btn btn-danger" onclick="return confirm('Are you sure to Delete This ?')">Delete</a> -->
+                    <a href="{{url('delete_post',$post->id)}}" class="btn btn-danger" onclick="confirmation(event)">Delete</a>
+                    <!-- <a href="{{url('delete_post',$post->id)}}" class="btn btn-danger"
+                        onclick="confirmation(event)">Delete</a> -->
                 </td>
             </tr>
-@endforeach
+            @endforeach
             <!-- <tr>
                 <td>ABCD</td>
                 <td>ABCD</td>
@@ -90,9 +98,38 @@ text-align:center;
                 <td>ABCD</td>
                 <td>ABCD</td>
             </tr> -->
-          </table>
+        </table>
 
-</div>
-       <!-- @include('admin.body') -->
-      @include('admin.footer')
+    </div>
+    <!-- @include('admin.body') -->
+    @include('admin.footer')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+      function confirmation(ev) {
+        ev.preventDefault();
+        var urlToRedirect = ev.currentTarget.getAttribute('href');
+        console.log(urlToRedirect);
+        swal({
+            title: "Are you sure to Delete this post",
+            text: "You will not be able to revert this!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+        .then((willCancel) => {
+            if (willCancel) {
+
+
+
+                window.location.href = urlToRedirect;
+
+            }
+
+
+        });
+
+
+    }
+</script>
+</body>
 </html>
